97f5d809a414ad30397197f4d10a00e6eec3c111
Okta Haris <oktaharis2008@gmail.com>
2025-07-06 18:39:40 +0700

    tes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./hooks/useAuth";
-import { ProtectedRoute } from "./components/Auth/ProtectedRoute";
+import { ProtectedRoute } from "./components/auth/ProtectedRoute";
 import { MainLayout } from "./components/layout/MainLayout";
 
 // Import all pages
